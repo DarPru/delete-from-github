b3ae13c121e857e622f430b1963b28e21b7c1aa7
DarPru <58913778+DarPru@users.noreply.github.com>
2023-06-22 19:57:51 +0400

    readme updeted + comments added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # delete-from-github
+
+This simple python code helps to delete users form Orgainsations and Repositories. Just simply put user needed to delete to **USERNAME** var and run the script. 
+
+Don't forget to install following lib:
+
+````
+pip install requests
+````
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 import json
 
+# Put you github token here
 YOUR_TOKEN = ""
+# Include the organisation name
 ORG = ''
+# Put the user you need to delete
 USERNAME = ''
 
 headers = {
@@ -11,6 +14,7 @@ headers = {
     "X-GitHub-Api-Version": "2022-11-28"
 }
 
+# Here if your organization has less then 100 repos you don't need to include last param to the quire 
 GET_REPO_LIST = f'https://api.github.com/orgs/{ORG}/repos?per_page=100'
 
 r = requests.get(GET_REPO_LIST, headers=headers)
@@ -24,6 +28,6 @@ for repo in repo_list:
     REPO = repo['name']
     URL = f'https://api.github.com/repos/{ORG}/{REPO}/collaborators/{USERNAME}'
     r = requests.delete(url=URL, headers=headers)
-    print(f'{i}Код ответа {r}: пользлватель {USERNAME} удален из {REPO}')
+    print(f'{i}Status code: {r}: user {USERNAME} deleted from {REPO}')
     i = i + 1
 
